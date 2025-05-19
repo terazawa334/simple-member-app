1206f93d8fe790c346913dd03f80e1b600639304
terazawa334 <h.terazawa.plusv@gmail.com>
2025-05-19 17:08:29 +0900

    簡単な修正

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function validateForm(){
-    const name = document.getElementById("name").Value.trim();
-    const email = document.getElementById("email").Value.trim();
-    const age = document.getElementById("age").Value.trim();
-}
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const age = document.getElementById("age").value.trim();
 
-if (name === "") {
-    alert("名前を入力してください。")
-    return false;
-}
+    if (name === "") {
+        alert("名前を入力してください。");
+        return false;
+    }
 
-if (email === "" || !email.includes("@")) {
-    alert("正しいメールアドレスを入力してください。")
-    return false;
-}
+    if (email === "" || !email.includes("@")) {
+        alert("正しいメールアドレスを入力してください。");
+        return false;
+    }
 
-if (age === "" || isNaN(age) || Number(age) < 0) {
-    alert("有効な年齢を入力してください。")
-    return false;
-}
+    if (age === "" || isNaN(age) || Number(age) < 0) {
+        alert("有効な年齢を入力してください。");
+        return false;
+    }
 
-alert("登録が完了しました。")
-return true;
+    alert("登録が完了しました。");
+    return true;
+}
